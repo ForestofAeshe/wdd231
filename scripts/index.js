42b264ee55a0153f60ad71ce5ac5b58ee35189a6
ForestofAeshe <msawpetersen@gmail.com>
2025-07-08 21:40:25 -0600

    testing hamburger

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,5 @@
-const menu = document.querySelector(".menu");
-const menuItems = document.querySelector(".menuItem");
-const hamburger = document.querySelector(".hamburger");
-const closeIcon = document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
-
-function toggleMenu() {
-    if (menu.classList.contains("showMenu")) {
-        menu.classList.remove("showMenu");
-        closeIcon.computedStyleMap.display = "none";
-        menuIcon.computedStyleMap.display = "block";
-    } else {
-        menu.classList.add("showMenu");
-        closeIcon.style.display = "block";
-        menuIcon.style.display = "none";
-    }
-}
-
-hamburger.addEventListener("click", toggleMenu);
-
-menuItems.forEach(
-    function(menuItemtem) {
-    item.addEventListener("click", toggleMenu);
-}
-)
+navbtn.addEventListener('click', () => {
+    navbtn.classList.toggle('show');
+    navLinks.classList.toggle('show');
+});
+ 
