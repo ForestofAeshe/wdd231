14412b6c69fdcd1cd3ef576d18089640e122a9b7
ForestofAeshe <msawpetersen@gmail.com>
2025-07-08 22:23:06 -0600

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,8 @@
+const navbtn = document.querySelector("#hamburger-button");
+const navLinks = document.querySelector("#navigation-bar");
+
+
 navbtn.addEventListener('click', () => {
     navbtn.classList.toggle('show');
     navLinks.classList.toggle('show');
 });
- 
