3e35267ad95ae64c8144745347b8e309ad51036d
ForestofAeshe <msawpetersen@gmail.com>
2025-07-11 21:23:51 -0600

    class cards

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,122 @@ navbtn.addEventListener('click', () => {
     navbtn.classList.toggle('show');
     navLinks.classList.toggle('show');
 });
+// ^^Menu of Hamburger
+
+const courses = [
+    {
+        subject: 'CSE',
+        number: 110,
+        title: 'Introduction to Programming',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.',
+        technology: [
+            'Python'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 130,
+        title: 'Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
+        technology: [
+            'HTML',
+            'CSS'
+        ],
+        completed: true
+    },
+    {
+        subject: 'CSE',
+        number: 111,
+        title: 'Programming with Functions',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
+        technology: [
+            'Python'
+        ],
+        completed: true
+    },
+    {
+        subject: 'CSE',
+        number: 210,
+        title: 'Programming with Classes',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.',
+        technology: [
+            'C#'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 131,
+        title: 'Dynamic Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 231,
+        title: 'Frontend Web Development I',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: false
+    }
+]
+// ^^Course List Array
+
+let courseList  = "";
+
+document.getElementById("all-courses").addEventListener("click", myFunction);
+document.getElementById("wdd-courses").addEventListener("click", myFunction);
+document.getElementById("cse-courses").addEventListener("click", myFunction);
+
+function myFunction() {
+
+    let courseButton = window.location.href.split("#")[1]
+    courses.filter((data) =>
+    {
+        if (courseButton === "All Courses") {
+            return data.courses > 2;
+
+        } else if (courseButton === "WDD Courses") {
+            return data.subject == "WDD";
+        } else if (courseButton === "CSE Courses") {
+            return data.subject == "CSE";
+        } else {
+            return data;
+        }
+    })
+
+    .map((data) =>
+    {
+        classList += `<div id="course-list">
+            <h2 class="course-name">${data.subject}${data.number}</h2>
+            <div class="course-complete">`
+            if (data.completed === true) {
+                "completed &check";
+            } else {
+                "not completed &#10007";
+            }
+            `</div>`
+    })
+}
