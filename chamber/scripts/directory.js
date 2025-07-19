69612d8cd9b92b0d79f5324299d3e5a9db4907ba
ForestofAeshe <msawpetersen@gmail.com>
2025-07-19 10:19:54 -0600

    grah, so frustrating

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,54 +1,72 @@
-const dataUrl = 'data/members.json';
+const dataPath = 'data/members.json';
 const card = document.querySelector('#member-cards');
 
 let allMembers = [];
 
-getMemberDirectory(dataUrl);
 
-document.getElementById('#all').addEventListener("load", getMemberDirectory(dataUrl));
-document.getElementById("#all").addEventListener("click", getMemberDirectory(dataUrl));
-document.getElementById("#1").addEventListener("click", getMemberDirectory(dataUrl));
-document.getElementById("#2").addEventListener("click", getMemberDirectory(dataUrl));
-document.getElementById("#3").addEventListener("click", getMemberDirectory(dataUrl));
+
+// document.getElementById('#all').addEventListener("load", getMemberDirectory(dataPath));
+// document.getElementById("#all").addEventListener("click", getMemberDirectory(dataPath));
+// document.getElementById("#1").addEventListener("click", getMemberDirectory(dataPath));
+// document.getElementById("#2").addEventListener("click", getMemberDirectory(dataPath));
+// document.getElementById("#3").addEventListener("click", getMemberDirectory(dataPath));
 
 async function getMemberDirectory(filePath) {
     const response = await fetch(filePath);
     const data = await response.json();
-    let allMembers = [];
+    // allMembers = [];
 
     console.table(data.members);
 
     allMembers = data.members;
-    displayMembers(allMembers);
+    displayMembers(data.allMembers);
 }
 
-function displayMembers (data) {
-    let cardList = "";
+getMemberDirectory(dataPath);
 
-    let cardButton = window.location.href.split("#")[1]
-    allMembers.filter((data) => {
+// function displayMembers (data) {
+//     let cardList = "";
 
-        switch (cardButton === "all") {
-            case "1":
-                data.membershipLevel === 1;
-                break;
-            case "2":
-                data.membershipLevel === 2;
-                break;
-            case "3":
-                data.membershipLevel === 3;
-                break;
-            default:
-                break;
+//     let cardButton = window.location.href.split("#")[1]
+//     allMembers.filter((data) => {
 
-        }}).map((data) => {
-            cardList += `<div class="member-cards" id="member-cards">
-            <h2 id="fullname">${data.fName} ${data.lName}</h2>
-            <p id="phone">${data.phone}</p>
-            <img src="${data.portrait}" alt="${data.fName} ${data.lName}" id="portrait">
-        </div>`;
-        })
+//         switch (cardButton === "#all") {
+//             case "#1":
+//                 data.membershipLevel === 1;
+//                 break;
+//             case "#2":
+//                 data.membershipLevel === 2;
+//                 break;
+//             case "#3":
+//                 data.membershipLevel === 3;
+//                 break;
+//             default:
+//                 break;
+
+//         }}).map((data) => {
+//             cardList += `<div class="member-cards" id="member-cards">
+//             <h2 id="fullname">${data.fName} ${data.lName}</h2>
+//             <p id="phone">${data.phone}</p>
+//             <img src="${data.portrait}" alt="${data.fName} ${data.lName}" id="portrait">
+//         </div>`;
+//         })
     
-    document.getElementById("member-cards").innerHTML = cardList;
+//     document.getElementById("member-cards").innerHTML = cardList;
 
-    }
+//     }
+
+const displayMembers = (members) => {
+    card.innerHTML = ``;
+    members.forEach(member => {
+        const memberDiv = document.createElement('section');
+        memberDiv.allMembers.add('member-card');
+        memberDiv.innerHTML = `
+            <h2 id="fullname">${member.fName} ${member.lName}</h2>
+            <p id="phone">${member.phone}</p>
+            <img src="${member.portrait}" alt="${member.fName} ${member.lName}" id="portrait">`;
+        
+        card.appendChild(memberDiv);
+    });   // document.getElementById("member-cards").allMembers.add('member-card');
+        
+    
+}
