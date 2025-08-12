b10d024857af04411d436e94b014349c2c097327
ForestofAeshe <msawpetersen@gmail.com>
2025-08-11 22:33:48 -0600

    work on join

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,1 +1,29 @@
-let orgInput = document.getElementById("organization-title")
+// let orgInput = document.getElementById("organization-title")
+
+function main() {
+    document.getElementById("myForm").addEventListener('submit',function(event) {
+        event.preventDefault();
+
+        const fname      = document.getElementById('fname').value;
+        const lname      = document.getElementById('lname').value;
+        const jobtitle   = document.getElementById('organization-title').value;
+        const email      = document.getElementById('email').value;
+        const phone      = document.getElementById('phone').value;
+        const orgname    = document.getElementById('org-name').value;
+        const membertier = document.getElementById('membership-level-dropdown').value;
+        orgdesc = document.getElementById('org-desc').value;    
+        const fullname   = fname + " " + lname;
+
+        const thankYouUrl = `thankyou.html?name=${encodeURIComponent(fullname)}&email=${encodeURIComponent(email)}`
+        window.location.href = thankYouUrl;
+        console.log(fullname);
+    })
+
+}
+
+
+
+
+
+
+main()
